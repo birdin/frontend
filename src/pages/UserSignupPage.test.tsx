f7fb0cd61321c807e35032e61c279996c82057cf
iCharlieAraiza <eliptikman1234@gmail.com>
2022-06-19 01:38:33 -0500

    Add TDD test cases

--- src/pages/UserSignupPage.test.tsx
+++ src/pages/UserSignupPage.test.tsx
@@ -1,17 +1,47 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { act } from 'react-dom/test-utils'
 import UserSignupPage from './UserSignupPage'
 
-describe('UserSignupPage form', () => {
+describe('UserSignupPage form elments', () => {
   it('should render the form', () => {
     render(<UserSignupPage />)
   })
-  
-  it('should render email input', ()=>{
-    const { getByLabelText } = render(<UserSignupPage />)
-    const emailInput = getByLabelText('Email')
-    expect(emailInput).toBeInTheDocument()
-  }) 
 
-  
+  it('should render email input', () => {
+    render(<UserSignupPage />)
+    expect(screen.getByPlaceholderText('Email')).toBeInTheDocument()
+  })
+
+  it('should render password input', () => {
+    render(<UserSignupPage />)
+    expect(screen.getByPlaceholderText('Password')).toBeInTheDocument()
+  })
+
+  it('should render confirm password input', () => {
+    render(<UserSignupPage />)
+    expect(screen.getByPlaceholderText('Confirm Password')).toBeInTheDocument()
+  })
 })
 
+describe('Valid data', () => {
+  it('Calls on submit function', async () => {
+    const mockOnSubmit = jest.fn()
+    render(<UserSignupPage onSubmit={mockOnSubmit} />)
+
+    await act(async () => {
+      fireEvent.change(screen.getByPlaceholderText('Email'), {
+        target: { value: 'helo@email.com' }
+      })
+      fireEvent.change(screen.getByPlaceholderText('Password'), {
+        target: { value: 'password' }
+      })
+      fireEvent.change(screen.getByPlaceholderText('Confirm Password'), {
+        target: { value: 'password' }
+      })
+    })
+    await act(async () => {
+      fireEvent.click(screen.getByRole("button"))
+    })
+    expect(mockOnSubmit).toHaveBeenCalled()
+  })
+})
--- src/pages/UserSignupPage.tsx
+++ src/pages/UserSignupPage.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const UserSignupPage = () => {
+const UserSignupPage = (props: any) => {
+
+  const onSubmitButton = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault()
+    props.onSubmit()
+    console.log('submit')
+  }
+
   return (
-        <div>UserSignupPage</div>
-        
+      <>
+        <form>
+          <label>Email</label>
+          <input type="email" placeholder="Email" />
+          <label>Password</label>
+          <input type="password" placeholder="Password" />
+          <label>Confirm Password</label>
+          <input type="password" placeholder="Confirm Password" />
+          <button type="submit" onClick={(e) => onSubmitButton(e)}>Sign Up</button>
+        </form>
+      </>  
   )
 }
 
